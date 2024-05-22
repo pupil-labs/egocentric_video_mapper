709128205dc648bb113b5e9fae923747cb2a4a06
sof <sof@pupil-labs.com>
2024-05-22 12:38:22 +0200

    Modifyng LOFTRImageMatcher to take patch boundaries instead of a point for image correspondences search

--- src/pupil_labs/action_cam_mapper/feature_matcher.py
+++ src/pupil_labs/action_cam_mapper/feature_matcher.py
@@ -12,19 +12,17 @@ class ImageMatcher(ABC):
 
 
 class LOFTRImageMatcher(ImageMatcher):
-    def __init__(self, location, gpu_num=None, patch_size=1000):
+    def __init__(self, location, gpu_num=None):
         if gpu_num is None:
             self.device= torch.device('cpu')
         else:
             self.device= torch.device(f'cuda:{gpu_num}' if torch.cuda.is_available() else 'cpu')
         self.image_matcher=kornia.feature.LoFTR(pretrained=location).to(self.device)
-        self.patch_size=patch_size
         self.transform=transforms.Compose([transforms.ToTensor()])
-        self.patch_corners=None
 
-    def get_correspondences(self, src_image, dst_image, neon_point=None):
+    def get_correspondences(self, src_image, dst_image, patch_corners=None):
         dst_tensor, dst_scaled2original = self._preprocess_image(dst_image)
-        src_image, self.patch_corners = self._get_image_patch(src_image, neon_point) if neon_point is not None else src_image.copy()
+        src_image = self._get_image_patch(src_image, patch_corners) if patch_corners is not None else src_image.copy()
         src_tensor, src_scaled2original = self._preprocess_image(src_image)
         input_dict = {
             "image0": src_tensor,
@@ -37,7 +35,7 @@ class LOFTRImageMatcher(ImageMatcher):
         for k in correspondences.keys():
             correspondences[k]=correspondences[k].cpu().numpy()
         correspondences=self._rescale_correspondences(correspondences, src_scaled2original, dst_scaled2original)
-        correspondences['keypoints0']=correspondences['keypoints0']+self.patch_corners[0]
+        correspondences['keypoints0']=correspondences['keypoints0']+patch_corners[0]
         return correspondences
 
     def _preprocess_image(self, image):
@@ -49,15 +47,15 @@ class LOFTRImageMatcher(ImageMatcher):
         scaled_image = torch.unsqueeze(scaled_image, dim=0)
         return scaled_image, ratio_scaled2image
 
-    def _get_image_patch(self, image, point):
-        patch_corners = self._get_patch_corners(self.patch_size, point, image.shape)
+    def _get_image_patch(self, image, patch_corners):
         x_min, y_min = min(patch_corners[:,0]), min(patch_corners[:,1])
         x_max, y_max = max(patch_corners[:,0]), max(patch_corners[:,1])
         image_patch = image[y_min:y_max,x_min:x_max,:]
-        return image_patch, patch_corners
+        return image_patch
     
     @staticmethod
     def _get_patch_corners(patch_size, point,image_shape):
+        point = point.reshape(-1)
         if point[0] < patch_size//2:
             x_min = 0
             x_max = patch_size
@@ -88,7 +86,7 @@ class LOFTRImageMatcher(ImageMatcher):
 class ImageMatcherFactory:
     def __init__(self,image_matcher, image_matcher_parameters):
         if image_matcher.upper() == 'LOFTR':
-            self.matcher = LOFTRImageMatcher(image_matcher_parameters['location'], image_matcher_parameters['gpu_num'], image_matcher_parameters['patch_size'])
+            self.matcher = LOFTRImageMatcher(**image_matcher_parameters)
         # does all the ifs, switchcases and param settings specific to each matcher to instantiate the desired image matcher
 
     def get_matcher(self):

389091a075316da34e60122f636e43e389ffb529
sof <sof@pupil-labs.com>
2024-05-23 09:56:56 +0200

    Change in naming

--- src/pupil_labs/action_cam_mapper/feature_matcher.py
+++ src/pupil_labs/action_cam_mapper/feature_matcher.py
@@ -20,12 +20,12 @@ class LOFTRImageMatcher(ImageMatcher):
         self.image_matcher=kornia.feature.LoFTR(pretrained=location).to(self.device)
         self.transform=transforms.Compose([transforms.ToTensor()])
 
-    def get_correspondences(self, src_image, dst_image, patch_corners=None):
+    def get_correspondences(self, src_image, dst_image, src_patch_corners=None): #add dst corner, 
         dst_tensor, dst_scaled2original = self._preprocess_image(dst_image)
-        src_image = self._get_image_patch(src_image, patch_corners) if patch_corners is not None else src_image.copy()
+        src_image = self._get_image_patch(src_image, src_patch_corners) if src_patch_corners is not None else src_image.copy()
         src_tensor, src_scaled2original = self._preprocess_image(src_image)
         input_dict = {
-            "image0": src_tensor,
+            "image0": src_tensor, 
             "image1": dst_tensor
         }
         for k in input_dict.keys():
@@ -35,7 +35,8 @@ class LOFTRImageMatcher(ImageMatcher):
         for k in correspondences.keys():
             correspondences[k]=correspondences[k].cpu().numpy()
         correspondences=self._rescale_correspondences(correspondences, src_scaled2original, dst_scaled2original)
-        correspondences['keypoints0']=correspondences['keypoints0']+patch_corners[0]
+        if src_patch_corners:
+            correspondences['keypoints0']=correspondences['keypoints0']+src_patch_corners[0]
         return correspondences
 
     def _preprocess_image(self, image):
@@ -52,30 +53,7 @@ class LOFTRImageMatcher(ImageMatcher):
         x_max, y_max = max(patch_corners[:,0]), max(patch_corners[:,1])
         image_patch = image[y_min:y_max,x_min:x_max,:]
         return image_patch
-    
-    @staticmethod
-    def _get_patch_corners(patch_size, point,image_shape):
-        point = point.reshape(-1)
-        if point[0] < patch_size//2:
-            x_min = 0
-            x_max = patch_size
-        elif point[0] < image_shape[1] - patch_size//2:
-            x_min = point[0] - patch_size//2
-            x_max = point[0] + patch_size//2
-        else:
-            x_min = image_shape[1] - patch_size
-            x_max = image_shape[1]
-        if point[1] < patch_size/2:
-            y_min = 0
-            y_max = patch_size
-        elif point[1] < image_shape[0] - patch_size//2:
-            y_min = point[1] - patch_size//2
-            y_max = point[1] + patch_size//2
-        else:
-            y_min = image_shape[0] - patch_size
-            y_max = image_shape[0]
-        return  np.array([[x_min,y_min],[x_min,y_max],[x_max,y_max],[x_max,y_min]], dtype=np.int32)
-    
+
     def _rescale_correspondences(self,correspondences, src_ratio, dst_ratio):
         correspondences['keypoints0'] = correspondences['keypoints0'] * src_ratio
         correspondences['keypoints1'] = correspondences['keypoints1'] * dst_ratio

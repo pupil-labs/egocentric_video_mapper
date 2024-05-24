11b8a6863ec9ade5fd0bba4118609b1835b4f06d
sof <sof@pupil-labs.com>
2024-05-24 11:12:22 +0200

    Added output_dir argument

--- src/pupil_labs/action_cam_mapper/example_script.py
+++ src/pupil_labs/action_cam_mapper/example_script.py
@@ -14,8 +14,10 @@ def get_file(folder_path, file_suffix='.mp4', required_in_name='0'):
             for name in files
             if name.endswith(file_suffix) and required_in_name in name][0]
 
-def calc_optic(neon_video,action_video,name,of_choice='farneback'):
-    Path(f'/users/sof/optic_flow/{name}').mkdir(parents=True, exist_ok=True)
+def calc_optic(neon_video,action_video,output_dir,of_choice='farneback'):
+    output_dir = Path(output_dir,'optic_flow')
+    Path(output_dir).mkdir(parents=True, exist_ok=True)
+
     if of_choice.lower() == 'farneback':
         action_of = OpticFlowCalculatorFarneback(video_dir=action_video)
         optic_flow_action=action_of.get_all_optic_flow()
@@ -28,8 +30,12 @@ def calc_optic(neon_video,action_video,name,of_choice='farneback'):
         optic_flow_neon = neon_of.get_all_optic_flow()
     else:
         raise ValueError('Invalid optic flow choice. Choose from "farneback" or "lk"')
-    action_of.write_to_csv(output_file=f'/users/sof/optic_flow/{name}/{name}_action_{of_choice}_of.csv')
-    neon_of.write_to_csv(output_file=f'/users/sof/optic_flow/{name}/{name}_neon_{of_choice}_of.csv')
+    
+    action_saving_path = Path(output_dir,f'action_{of_choice}_of.csv')
+    neon_saving_path = Path(output_dir,f'neon_{of_choice}_of.csv')
+    action_of.write_to_csv(output_file=action_saving_path)
+    neon_of.write_to_csv(output_file=neon_saving_path)
+    return action_saving_path,neon_saving_path
 
 def align_videos(action_result,neon_result,action_vid_path,neon_timestamps):
     offset_calc=OffsetCalculator(source=action_result['angle'].values,source_timestamps=action_result['start'].values, destination=neon_result['angle'].values, destination_timestamps=neon_result['start'].values,resampling_frequency=500)
@@ -43,11 +49,17 @@ def main_mapper(action_vid_path,
                 neon_timestamps,
                 action_timestamps,
                 neon_gaze_csv,
-                name,
+                neon_opticflow_csv,
+                action_opticflow_csv,
+                output_dir,
                 matcher,
                 ):
+    output_dir=Path(output_dir,matcher['choice'])
+    Path(output_dir).mkdir(parents=True, exist_ok=True)
+
     param=matcher['parameters']
     print(f'Using {matcher["choice"]} with parameters: {param}')
+
     mapper = ActionCameraGazeMapper(neon_gaze_csv=neon_gaze_csv,
         neon_video_dir=neon_vid_path,
         action_video_dir=action_vid_path,
@@ -55,35 +67,43 @@ def main_mapper(action_vid_path,
         action_worldtimestamps=action_timestamps,
         image_matcher=matcher['choice'],
         image_matcher_parameters=param,
-        neon_opticflow_csv=f'/users/sof/optic_flow/{name}/{name}_neon_lk_of.csv',
-        action_opticflow_csv=f'/users/sof/optic_flow/{name}/{name}_action_lk_of.csv',
+        neon_opticflow_csv=neon_opticflow_csv,
+        action_opticflow_csv=action_opticflow_csv,
         patch_size=1000)
-    Path(f'/users/sof/mapped_gaze/{name}').mkdir(parents=True, exist_ok=True)
-    mapper.map_gaze(saving_path=f'/users/sof/mapped_gaze/{name}/{name}_gaze_mapping_{matcher["choice"]}.csv')
+    
+    mapper.map_gaze(saving_path=Path(output_dir,'action_gaze.csv'))
 
-def main(action_vid_path, neon_timeseries_dir, image_matcher,optic_flow_choice='lk',render_video=False):
+def main(action_vid_path, neon_timeseries_dir, output_dir, image_matcher,optic_flow_choice='lk',render_video=False):
+
     neon_vid_path=get_file(neon_timeseries_dir, file_suffix='.mp4')
     neon_timestamps=neon_timeseries_dir+'/world_timestamps.csv'
     neon_gaze_csv=neon_timeseries_dir+'/gaze.csv'
-    name=Path(action_vid_path).parent.name
+
+    Path(output_dir).mkdir(parents=True, exist_ok=True)
+    
     #Step 1: Calculate optic flow
-    calc_optic(neon_video=neon_vid_path,action_video=action_vid_path,name=name,of_choice=optic_flow_choice)
+    action_of_path, neon_of_path=calc_optic(neon_video=neon_vid_path,action_video=action_vid_path,output_dir=output_dir,of_choice=optic_flow_choice)
     print('Optic flow for both videos calculated')
+
     #Step 2: Estimate time offset and create action world timestamps
-    action_result=pd.read_csv(f'/users/sof/optic_flow/{name}/{name}_action_{optic_flow_choice}_of.csv')
-    neon_result=pd.read_csv(f'/users/sof/optic_flow/{name}/{name}_neon_{optic_flow_choice}_of.csv')
+    action_result=pd.read_csv(action_of_path)
+    neon_result=pd.read_csv(neon_of_path)
     align_videos(action_result,neon_result,action_vid_path,neon_timestamps)
-    #check is world_timestamps.csv is created
+
+    #check if world_timestamps.csv is created
     action_timestamps=neon_timeseries_dir+'/action_camera_world_timestamps.csv'
     if not Path(action_timestamps).exists():
         raise FileNotFoundError(f'{action_timestamps} not created!')
+    
     #Step 3: Map gaze
     main_mapper(action_vid_path=action_vid_path,
                 neon_vid_path=neon_vid_path,
                 neon_timestamps=neon_timestamps,
                 action_timestamps=action_timestamps,
                 neon_gaze_csv=neon_gaze_csv,
-                name=name,
+                neon_opticflow_csv=neon_of_path,
+                action_opticflow_csv=action_of_path,
+                output_dir=Path(output_dir,'mapped_gaze'),
                 matcher=image_matcher
                 )
     #Step 4 (Optional): Render simultaneous videos with gaze in both
@@ -94,12 +114,19 @@ def main(action_vid_path, neon_timeseries_dir, image_matcher,optic_flow_choice='
 if __name__ == "__main__":
     action_vid_path='/users/sof/gaze_mapping/raw_videos/InstaVid/wearingNeon_2m/AVun_20240216_160246_055.mp4'
     neon_timeseries_path='/users/sof/gaze_mapping/raw_videos/Neon/Raw_Data/2024-02-16_wearingNeon/2024-02-16_15-58-13-6310bec3/'
+
+    output_dir='/users/sof/action_map_experiments/' # parent directory for all outputs
+    name=Path(action_vid_path).parent.name
+    output_dir=Path(output_dir,name)
+
     param_lg = {'num_features':2048,'gpu_num':0}
     param_loftr = {'location':'indoor', 'gpu_num':0}
     image_matcher_loftr={'choice':'loftr','parameters':param_loftr}
     image_matcher_lg={'choice':'disk_lightglue','parameters':param_lg}
+
     main(action_vid_path=action_vid_path, 
-        neon_timeseries_dir=neon_timeseries_path, 
+        neon_timeseries_dir=neon_timeseries_path,
+        output_dir=output_dir, 
+        image_matcher=image_matcher_lg,
         optic_flow_choice='lk', 
-        image_matcher=image_matcher_lg , 
         render_video=False)

acb7058e0ce85f99333b7191b17feb73399e9a42
sof <sof@pupil-labs.com>
2024-07-05 14:08:30 +0200

    Adding EfficientLoFTRImageMatcher class

--- src/pupil_labs/action_cam_mapper/feature_matcher.py
+++ src/pupil_labs/action_cam_mapper/feature_matcher.py
@@ -199,6 +199,99 @@ class DISK_LightGlueImageMatcher(ImageMatcher):
         return scaled_image, ratio_scaled2image
 
 
+class EfficientLoFTRImageMatcher(ImageMatcher):
+    def __init__(self, model_type="opt", precision="fp32", gpu_num=None):
+        if gpu_num is None:
+            self.device = torch.device("cpu")
+        else:
+            self.device = torch.device(
+                f"cuda:{gpu_num}" if torch.cuda.is_available() else "cpu"
+            )
+
+        if model_type == "full":
+            _default_cfg = deepcopy(full_default_cfg)
+        elif model_type == "opt":
+            _default_cfg = deepcopy(opt_default_cfg)
+
+        self.precision = precision
+        if self.precision == "mp":
+            _default_cfg["mp"] = True
+        elif self.precision == "fp16":
+            _default_cfg["half"] = True
+
+        self.image_matcher = eLoFTR(config=_default_cfg)
+        self.image_matcher.load_state_dict(
+            torch.load("./efficient_loftr/weights/eloftr_outdoor.ckpt")["state_dict"]
+        )
+        self.image_matcher = reparameter(self.image_matcher)
+        if precision == "fp16":
+            self.image_matcher = self.image_matcher.half()
+        self.image_matcher = self.image_matcher.eval().to(self.device)
+
+    def get_correspondences(
+        self, src_image, dst_image, src_patch_corners=None, dst_patch_corners=None
+    ):
+        dst_image = (
+            self._get_image_patch(dst_image, dst_patch_corners)
+            if dst_patch_corners is not None
+            else dst_image.copy()
+        )
+        dst_tensor, dst_scaled2original = self._preprocess_image(dst_image)
+
+        src_image = (
+            self._get_image_patch(src_image, src_patch_corners)
+            if src_patch_corners is not None
+            else src_image.copy()
+        )
+        src_tensor, src_scaled2original = self._preprocess_image(src_image)
+        batch = {"image0": src_tensor, "image1": dst_tensor}
+        for k in batch.keys():
+            batch[k] = batch[k].to(self.device)
+
+        with torch.inference_mode():
+            if self.precision == "mp":
+                with torch.autocast(enabled=True, device_type="cuda"):
+                    self.image_matcher(batch)
+            else:
+                self.image_matcher(batch)
+
+        correspondences = {
+            "keypoints0": batch["mkpts0_f"].cpu().numpy(),
+            "keypoints1": batch["mkpts1_f"].cpu().numpy(),
+            "confidence": batch["mconf"].cpu().numpy(),
+        }
+        correspondences = self._rescale_correspondences(
+            correspondences, src_scaled2original, dst_scaled2original
+        )
+        if src_patch_corners is not None:
+            correspondences["keypoints0"] = (
+                correspondences["keypoints0"] + src_patch_corners[0]
+            )
+
+        if dst_patch_corners is not None:
+            correspondences["keypoints1"] = (
+                correspondences["keypoints1"] + dst_patch_corners[0]
+            )
+
+        return correspondences
+
+    def _preprocess_image(self, image):
+        image = cv.cvtColor(image, cv.COLOR_BGR2GRAY)
+        scaled_image = cv.resize(
+            image, (image.shape[1] // 56 * 32, image.shape[0] // 56 * 32)
+        )
+        ratio_scaled2image = (
+            image.shape[1] / scaled_image.shape[1],
+            image.shape[0] / scaled_image.shape[0],
+        )
+        if self.precision == "fp16":
+            scaled_image = torch.from_numpy(scaled_image)[None][None].half() / 255.0
+        else:
+            scaled_image = torch.from_numpy(scaled_image)[None][None] / 255.0
+
+        return scaled_image, ratio_scaled2image
+
+
 class ImageMatcherFactory:
     def get_matcher(self, image_matcher, image_matcher_parameters):
         if image_matcher.upper() == "LOFTR":

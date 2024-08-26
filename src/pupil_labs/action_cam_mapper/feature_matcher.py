d447b09c0796822d8fdec56f43ef92291a406146
sof <sof@pupil-labs.com>
2024-08-26 16:28:43 +0200

    Adding gpu_num to init of base class

--- src/pupil_labs/action_cam_mapper/feature_matcher.py
+++ src/pupil_labs/action_cam_mapper/feature_matcher.py
@@ -1,16 +1,25 @@
-import cv2 as cv
-import numpy as np
-import kornia
-import torch
-from torchvision import transforms
-from copy import deepcopy
 from abc import ABC, abstractmethod
+from copy import deepcopy
+from pathlib import Path
+
+import cv2 as cv
+import kornia
+import numpy as np
+import torch
 from efficient_loftr.src.loftr import LoFTR as eLoFTR
 from efficient_loftr.src.loftr import full_default_cfg, opt_default_cfg, reparameter
-from pathlib import Path
+from torchvision import transforms
 
 
 class ImageMatcher(ABC):
+    def __init__(self, gpu_num=None):
+        if gpu_num is None:
+            self.device = torch.device("cpu")
+        else:
+            self.device = torch.device(
+                f"cuda:{gpu_num}" if torch.cuda.is_available() else "cpu"
+            )
+
     @abstractmethod
     def get_correspondences(
         self, src_image, dst_image, src_patch_corners=None, dst_patch_corners=None
@@ -37,12 +46,7 @@ class LOFTRImageMatcher(ImageMatcher):
             location (str):Pretrained weights of LOFTR in ['indoor', 'outdoor'].'outdoor' is trained on MegaDepth dataset and 'indoor' on ScanNet dataset.
             gpu_num (int, optional): The GPU device number to use, if None it uses CPU. Defaults to None.
         """
-        if gpu_num is None:
-            self.device = torch.device("cpu")
-        else:
-            self.device = torch.device(
-                f"cuda:{gpu_num}" if torch.cuda.is_available() else "cpu"
-            )
+        super().__init__(gpu_num)
         self.image_matcher = kornia.feature.LoFTR(pretrained=location).to(self.device)
         self.transform = transforms.Compose([transforms.ToTensor()])
 
@@ -108,14 +112,9 @@ class DISK_LightGlueImageMatcher(ImageMatcher):
         This class is a wrapper for the LightGlue algorithm with DISK features from Kornia library. It is used to find correspondences between two images. To read about both algorithms, please refer to DISK: Learning local features with policy gradient (https://github.com/cvlab-epfl/disk) and to LightGlue: Local Feature Matching at Light Speed (https://github.com/cvg/LightGlue?tab=readme-ov-file)
         Args:
             num_features (int): The maximum number of keypoints in DISK to detect. If None, all keypoints are returned.
-            gpu_num (int, optional): The GPU device number to use, if None it uses CPU. Defaults to None.
+            gpu_num (int, optional): The GPU device number to use, if None it uses CPU. Defaults to None
         """
-        if gpu_num is None:
-            self.device = torch.device("cpu")
-        else:
-            self.device = torch.device(
-                f"cuda:{gpu_num}" if torch.cuda.is_available() else "cpu"
-            )
+        super().__init__(gpu_num)
         self.num_features = num_features
 
         self.feature_extractor = kornia.feature.DISK.from_pretrained("depth").to(
@@ -220,12 +219,7 @@ class DeDoDe_LightGlueImageMatcher(ImageMatcher):
             num_features (int): The maximum number of keypoints in DeDoDe to detect. Defaults to 10000.
             gpu_num (int, optional): The GPU device number to use, if None it uses CPU. Defaults to None.
         """
-        if gpu_num is None:
-            self.device = torch.device("cpu")
-        else:
-            self.device = torch.device(
-                f"cuda:{gpu_num}" if torch.cuda.is_available() else "cpu"
-            )
+        super().__init__(gpu_num)
         self.num_features = num_features
 
         self.feature_extractor = kornia.feature.DeDoDe.from_pretrained(
@@ -332,13 +326,7 @@ class EfficientLoFTRImageMatcher(ImageMatcher):
             model_type (str, optional): Model type in ['full', 'opt']. Use 'full' for best quality, 'opt' for best efficiency. Defaults to "opt".
             gpu_num (int, optional): The GPU device number to use, if None it uses CPU. Defaults to None.
         """
-        if gpu_num is None:
-            self.device = torch.device("cpu")
-        else:
-            self.device = torch.device(
-                f"cuda:{gpu_num}" if torch.cuda.is_available() else "cpu"
-            )
-
+        super().__init__(gpu_num)
         if model_type == "full":
             _default_cfg = deepcopy(full_default_cfg)
         elif model_type == "opt":
